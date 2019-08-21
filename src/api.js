b3f002a221171970245a6a5f05aa4b60891a809c
mbildner <mbildner@icloud.com>
2019-08-21 15:17:24 -0400

    Find workaround solution to reference error, refactor/move data to separate function

--- src/api.js
+++ src/api.js
@@ -1,19 +1,52 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import "babel-polyfill";
+// require("babel-core/register");
+// require("babel-polyfill");
+// import "@babel/polyfill";
+
+// query example	== https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=AAPL&api_key=-JuketAYn-cNXiDioYKb
+// query syntax 	== https://www.quandl.com/api/v3/datatables/${tableName}.${dataFormat}?ticker=AAPL&api_key=-JuketAYn-cNXiDioYKb
+// doc: https://www.quandl.com/databases/SF1/documentation?anchor=getting-started
+// https://jsonformatter.curiousconcept.com/
+
+// "use strict";
+// async function getData(ticker) {
+//     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//     const API_KEY = '-JuketAYn-cNXiDioYKb';
+//     const stockUrl = `https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=${ticker}&api_key=${API_KEY}`;
+//     const response = await fetch(proxyurl + stockUrl);
+//     return response.json()
+// }
+// export async function getStock(ticker) {
+//     const data = await getData(ticker);
+//     return data;
+// }
+
+
 
 export function getStock(ticker) {
-    const StockUrl = `https://max.herokuapp.com/stocks/${API_KEY}/${ticker}`;
-    //const StockUrl = `https://www.quandl.com/api/v3/datasets/YAHOO/MSFT-MSFT-Microsoft-Corporation.json?api_key=-JuketAYn-cNXiDioYKb`; //`https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=${ticker}&api_key=-JuketAYn-cNXiDioYKb`;
-    const req = new Request(StockUrl, {
-        method: 'GET',
-        mode: 'no-cors',
-    });
-    return fetch(req)
-    // return axios.get(`https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=${ticker}&api_key=-JuketAYn-cNXiDioYKb`,
-    // { 
-    //     mode: 'no-cors',
-    //     headers: { 'Access-Control-Allow-Origin': '*' } 
-    // })
-    .catch(function (error) {
-        console.log(error);
-    });
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const API_KEY = '-JuketAYn-cNXiDioYKb';
+    const stockUrl = `https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=${ticker}&api_key=${API_KEY}`;
+    return fetch(proxyurl + stockUrl).then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        })        
 }
+
+// let data = await getStock('AAPL');  //=> {data: Array(8), columns: Array(111)}  
+// data refers to columns, array 111 represents columns
+
+// const StockUrl = `https://max.herokuapp.com/stocks/${API_KEY}/${ticker}`;
+// return axios.get(`https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.json?ticker=${ticker}&api_key=-JuketAYn-cNXiDioYKb`,
+// { 
+//     mode: 'no-cors',
+//     headers: { 'Access-Control-Allow-Origin': '*' } 
+// })
+// .catch(function (error) {
+//     console.log(error);
+// });
+
+
